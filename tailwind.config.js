005dd0dff66202ad24fbff68c3ef2d3114b0e1bb
Iroshan De Zilva <55875936+iroshandezilva@users.noreply.github.com>
2021-09-18 13:30:09 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [
+      './public/**/*.php',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "15px"
+    },
+
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
